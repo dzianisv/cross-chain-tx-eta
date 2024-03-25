a2477bfaa4adcfe768bee267f8d369abc14b3e6a
Den <denis_v@tuta.io>
2024-03-24 20:35:13 -0400

    Add samples

---
+++ samples/chains.js
@@ -0,0 +1,20 @@
+await fetch("https://api.superform.xyz/supported/chains", {
+    "credentials": "omit",
+    "headers": {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:124.0) Gecko/20100101 Firefox/124.0",
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Language": "en-US,en;q=0.5",
+        "cache-control": "public, s-maxage=1200, stale-while-revalidate=600",
+        "response-content-type": "application/json",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-site"
+    },
+    "referrer": "https://scan.superform.xyz/",
+    "method": "GET",
+    "mode": "cors"
+});
+
+
+
+
---
+++ samples/tx.js
@@ -0,0 +1,17 @@
+await fetch("https://scan.superform.xyz/tx/0x081456c377472e06505981d2c66b22e818fb534358e3cd8e371177ca10dec20d/?superformId=62771017355604367871395387074880950584551153706402526930595&_rsc=3ored", {
+    "credentials": "include",
+    "headers": {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:124.0) Gecko/20100101 Firefox/124.0",
+        "Accept": "*/*",
+        "Accept-Language": "en-US,en;q=0.5",
+        "RSC": "1",
+        "Next-Router-State-Tree": "%5B%22%22%2C%7B%22children%22%3A%5B%22tx%22%2C%7B%22children%22%3A%5B%220x081456c377472e06505981d2c66b22e818fb534358e3cd8e371177ca10dec20d%22%2C%7B%22children%22%3A%5B%22%22%2C%7B%22children%22%3A%5B%22__PAGE__%22%2C%7B%7D%5D%7D%5D%7D%5D%7D%2Cnull%2C%22refetch%22%5D%7D%2Cnull%2Cnull%2Ctrue%5D",
+        "Next-Url": "/",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin"
+    },
+    "referrer": "https://scan.superform.xyz/",
+    "method": "GET",
+    "mode": "cors"
+});
